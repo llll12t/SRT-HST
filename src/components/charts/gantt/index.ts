2457f509b24315aab64169ad797470e84ede3469
III12N <147978645+llll12t@users.noreply.github.com>
2026-01-27 01:07:52 +0700

    Update index.ts

--- src/components/charts/gantt/index.ts
+++ src/components/charts/gantt/index.ts
@@ -2,7 +2,7 @@
 export { default as GanttChart } from './GanttChart';
 export { default as GanttToolbar } from './GanttToolbar';
 export { default as TimelineHeader } from './TimelineHeader';
-export { default as SCurveOverlay } from './SCurveOverlay';
 
 // Types
 export * from './types';
+
