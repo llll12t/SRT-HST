db55712bb899d6e84c44cc35cfde8d0b8b66fa44
III12N <147978645+llll12t@users.noreply.github.com>
2026-01-27 01:10:59 +0700

    Update index.ts

--- src/components/charts/index.ts
+++ src/components/charts/index.ts
@@ -1,6 +1,7 @@
 // Charts Module - Re-export all chart components
 
 // Gantt
-export { GanttChart, GanttToolbar, TimelineHeader, SCurveOverlay } from './gantt';
+export { GanttChart, GanttToolbar, TimelineHeader } from './gantt';
 export * from './gantt/types';
+
 
