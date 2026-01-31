733a15e8ffa372a5d9e8e25f3ebf5ca01cfb6811
III12N <147978645+llll12t@users.noreply.github.com>
2026-01-31 17:39:33 +0700

    Delete src directory

--- src/components/charts/gantt/index.ts
+++
@@ -1,8 +0,0 @@
-// Gantt Chart Components
-export { default as GanttChart } from './GanttChart';
-export { default as GanttToolbar } from './GanttToolbar';
-export { default as TimelineHeader } from './TimelineHeader';
-
-// Types
-export * from './types';
-
--- src/components/charts/index.ts
+++
@@ -1,7 +0,0 @@
-// Charts Module - Re-export all chart components
-
-// Gantt
-export { GanttChart, GanttToolbar, TimelineHeader } from './gantt';
-export * from './gantt/types';
-
-
